89006052653304ad57f4772a96794c37e013ec58
sirishrenukumar <sirish.renukumar@gmail.com>
2015-07-14 15:19:13 +0530

    Duplicate beans and naming conventions

---
+++ 1c-standalone-naming-beans/src/main/java/com/skbr/MessageGenerator.java
@@ -0,0 +1,9 @@
+package com.skbr;
+
+public class MessageGenerator {
+	
+	public String getMessage() {
+		return "Hello";
+	}
+
+}
---
+++ 1c-standalone-naming-beans/src/main/java/com/skbr/StandaloneApplication.java
@@ -0,0 +1,22 @@
+package com.skbr;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class StandaloneApplication {
+
+	public static void main(String[] args) {
+		
+
+		ApplicationContext applicationContext1 = new ClassPathXmlApplicationContext("beans.xml");
+		MessageGenerator messageGenerator1 = applicationContext1.getBean(MessageGenerator.class);
+
+		System.out.println(messageGenerator1.getMessage());
+
+		ApplicationContext applicationContext2 = new ClassPathXmlApplicationContext("beans.xml");
+		MessageGenerator messageGenerator2 = applicationContext2.getBean(MessageGenerator.class);
+
+		System.out.println(messageGenerator2.getMessage());
+
+	}
+}
